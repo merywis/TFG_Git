c3503c451eab6a3121d77bc5e846124ba32bc4f6
Maria Orell Monserrat <mariaorell2000@gmail.com>
2022-03-05 01:16:24 +0100

    pop up box quasi fet

--- javaScript/funcions.js
+++ javaScript/funcions.js
@@ -6,7 +6,6 @@
 var boxMenu = document.getElementById("boxTextoMenu");
 boxMenu.addEventListener('click', function () {
   boxMenu.parentNode.removeChild(boxMenu);
-  console.log("hola")
 });
 
 
@@ -65,113 +64,48 @@ function crearGaleria() {
 
 
         /* VENTANA POP UP ALS QUADRES */
-        var Overlay = document.createElement("div");
-        Overlay.setAttribute('class', "overlay");
-        Overlay.setAttribute('id', "overlay");
-
-        var popUp = document.createElement("div");
-        popUp.setAttribute('class', "popup");
-        popUp.setAttribute('id', "popup");
-        Overlay.appendChild(popUp);
-
-        var btnTancar = document.createElement("a");
-        btnTancar.setAttribute('class', "btn-cerrar-popup");
-        btnTancar.setAttribute('id', "btn-cerrar-popup");
-        btnTancar.setAttribute('href', "#");
-        popUp.appendChild(btnTancar);
-
-        var btnIcono = document.createElement("i");
-        btnIcono.setAttribute('class', "fas fa-times");
-        btnTancar.appendChild(btnIcono);
-
-        /* *****començam amb el contingut del pop up *** */
-
-        var contingutPopUP = document.createElement("div");
-        contingutPopUP.setAttribute('class', "contenedor");
-        // texteGran.setAttribute('id', "popup");
-        popUp.appendChild(contingutPopUP);
-
-        /* ANTES var titol = document.createElement("h3");
-        var newContent = document.createTextNode("Hola!!!¿Qué tal?");
-        titol.appendChild(newContent);
-        popUp.appendChild(titol);*/
-        
-        var headerPrincipal = document.createElement("header");
-        headerPrincipal.setAttribute('class',"header");
-        var newContent = document.createTextNode("ESPAI DEL HEADER");
-        headerPrincipal.appendChild(newContent);
-        contingutPopUP.appendChild(headerPrincipal);
-
-        var contingutEsq = document.createElement("div");
-        contingutEsq.setAttribute('class', "contenido");
-        contingutPopUP.appendChild(contingutEsq);
-
-        var titolEsq = document.createElement("h1");
-        var newContent4 = document.createTextNode("Contenido esquerra");
-        titolEsq.appendChild(newContent4);
-        contingutEsq.appendChild(titolEsq);
-
-        var contingutDreta = document.createElement("div");
-        contingutDreta.setAttribute('class', "sidebar");
-        contingutPopUP.appendChild(contingutDreta);
-        
-        var titolDreta = document.createElement("h3");
-        var newContent5 = document.createTextNode("Contenido dreta");
-        titolDreta.appendChild(newContent5);
-        contingutDreta.appendChild(titolDreta);
-
-        var footer = document.createElement("footer");
-        footer.setAttribute('class', "footer");
-        contingutPopUP.appendChild(footer);
-
-        var titolFooter = document.createElement("h3");
-        var newContent6 = document.createTextNode("Contenido footer");
-        titolFooter.appendChild(newContent6);
-        footer.appendChild(titolFooter);
-
-        /*var titol2 = document.createElement("h4");
-        var newContent2 = document.createTextNode("Info sobre el contingut quadre");
-        titol2.appendChild(newContent2);
-        popUp.appendChild(titol2);*/
         /*
+         var btnTancar = document.createElement("a");
+         btnTancar.setAttribute('class', "btn-cerrar-popup");
+         btnTancar.setAttribute('id', "btn-cerrar-popup");
+         btnTancar.setAttribute('href', "#");
+         popUp.appendChild(btnTancar);
+ 
+         var btnIcono = document.createElement("i");
+         btnIcono.setAttribute('class', "fas fa-times");
+         btnTancar.appendChild(btnIcono);*/
 
-        <div class="contenedor">
-    <header class="header">
-      <h2>HEADER</h2>
-    </header>
-    <main class="contenido">
-      <h1>Contenido</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis rutrum gravida. Aliquam vel nunc sit amet nibh aliquam sollicitudin eu vitae elit. Duis varius turpis est, at feugiat metus blandit non. Mauris est nunc, ullamcorper nec egestas at, faucibus ac ex. Cras gravida ut odio eget vulputate. Suspendisse ut nunc cursus, vulputate tortor id, mollis magna. Proin mattis euismod magna. Suspendisse mattis, nunc vitae mattis iaculis, elit massa facilisis magna, ac consequat magna lacus sit amet lectus. Suspendisse a lacinia est, a semper turpis. Phasellus lobortis eget nibh in scelerisque. Morbi feugiat volutpat nisl, vehicula commodo augue volutpat at. Aenean aliquet tristique diam. Aenean maximus, quam non sollicitudin efficitur, est sapien pharetra odio, eget aliquam justo urna eu eros. Donec nec tincidunt tortor.
-      </p>
-    </main>
-    <aside class="sidebar">
-      <h3>SIDEBAR</h3>
-    </aside>
-    <div class="widget-1">
-      <h3>WIDGET 1</h3>
-    </div>
-    <div class="widget-2">
-      <h3>WIDGET 2</h3>
-    </div>
-    <footer class="footer">
-      <h3>FOOTER</h3>
-    </footer>
-  </div>
-        */
 
-        document.getElementById("sala").append(Overlay);
 
         boxQuadre.addEventListener('click', function () {
-          Overlay.classList.add('active');
-          popUp.classList.add('active');
-        });
+          var pos = document.getElementById("camera").getAttribute('position');
+          var rotationCamera = document.getElementById("camera").getAttribute('rotation');
+          /* var cameraEl = document.querySelector('#camera');
+          var worldPos = new THREE.Vector3();
+          worldPos.setFromMatrixPosition(cameraEl.object3D.matrixWorld);
+          console.log(worldPos.x); */
+          console.log("Rotació camara:");
+          console.log(rotationCamera);
+          var boxPopUp = document.createElement("a-box");
+          boxPopUp.setAttribute('color', "purple");
+          var XposBox = Math.sin(rotationCamera.y) + pos.x;
+          var ZposBox = Math.cos(rotationCamera.y) + pos.z;
+          boxPopUp.setAttribute('position', { x: XposBox, y: pos.y, z: ZposBox});
+          console.log("Posició box:");
+          console.log(pos.x, pos.y, pos.z);
+          boxPopUp.setAttribute('rotation', rotationCamera);
+          boxPopUp.setAttribute('depth', 0.5);
+          boxPopUp.setAttribute('width', 5);
+          boxPopUp.setAttribute('height', 3.5);
+          document.getElementById("sala").append(boxPopUp); //jaume diu q append a camera i no a sala
 
-        btnTancar.addEventListener('click', function (e) {
-          e.preventDefault(); //si llev aquesta línea, he de llevarli la "e" com a paràmetre
-          Overlay.classList.remove('active');
-          popUp.classList.remove('active');
         });
+        /*
+                btnTancar.addEventListener('click', function (e) {
+                  e.preventDefault(); //si llev aquesta línea, he de llevarli la "e" com a paràmetre
+                  Overlay.classList.remove('active');
+                  popUp.classList.remove('active');
+                });*/
 
       });
     }
